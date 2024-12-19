bd27932336103f575bb8d70202b348f7442bba6c
saurabh-kd-singh <sksingh5087@gmail.com>
2024-12-19 14:31:36 +0530

    First array problem : Finding the second largest in an array.

--- Arrays/1.Second Largest/secondLargest.cpp
+++ Arrays/1.Second Largest/secondLargest.cpp
@@ -1,5 +1,34 @@
+#include <vector>
+#include <iostream>
 using namespace std;
 
+int getSecondLargest(vector<int> &arr) {
+        int largest=arr[0];
+        int secondLargest=arr[0];
+        for(int i=1; i<arr.size(); i++) {
+            if(arr[i]>largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+            else if(largest==secondLargest && arr[i]<largest) {
+                secondLargest = arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        if(largest==secondLargest) {
+            return -1;
+        } else {
+            return secondLargest;
+        }
+    }
+
 int main() {
-    return 0;
+    vector<int> arr = {1, 2 , 3, 4, 5};
+    cout << "Enter a number";
+    int test;
+    cin >> test;
+    cout << getSecondLargest(arr);
+    return 1;
 }
